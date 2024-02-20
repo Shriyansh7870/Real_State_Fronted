bf6efca9773eff7f0139f449a54e346a12b97a39
Shriumenit <s.kumar@outlook.com>
2024-02-20 16:11:48 +0530

    Second Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./Component/Header";
 function App() {
   return (
     <>
-      <div>
-        <p className="text-red-500">Hii Shriyansh this side</p>
-      </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-out" element={<SignOut />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
